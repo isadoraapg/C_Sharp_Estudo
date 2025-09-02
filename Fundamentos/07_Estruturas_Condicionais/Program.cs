3d781c38e0b22c37468f350d9ccb4c07d951ca9a
Isadora Peixoto <133281545+isadoraapg@users.noreply.github.com>
2025-09-02 16:35:55 -0300

    Add files via upload

---
+++ Fundamentos/07_Estruturas_Condicionais/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//FUNDAMENTOS DO C# 
+
+//ESTRUTURAS CONDICIONAIS
+
+var a = 3;
+var b = 2;
+
+if (a > b)
+{
+    Console.WriteLine("É maior");
+}
+else
+{
+    Console.WriteLine("Não é maior");
+}
+
+
+var nota = 7.5m;
+
+if (nota >= 4 && nota < 7)
+{
+    Console.WriteLine("Nota = " + nota + " - Recuperação");
+}
+else if (nota >= 7)
+{
+    Console.WriteLine("Nota = " + nota + " - Aprovado");
+}
+else
+{
+    Console.WriteLine("Nota = " + nota + " - Reprovado");
+}
+
+
+//operador ternário 
+
+var nota1 = 5;
+var mensagem = "";
+
+if (nota1 >= 7)
+{
+    mensagem = "aprovado";
+}
+else
+{
+    mensagem = "reprovado";
+}
+Console.WriteLine(mensagem);
+
+//condição ? valor se verdadeiro : valor se falso
+mensagem = nota1 >= 7 ? "Aprovado" : "Reprovado";
+Console.WriteLine(mensagem);
+
+//Switch
+Console.WriteLine("Digite uma opção de 1 a 3: ");
+var opcao = Console.ReadLine();
+switch (opcao)
+{
+    case "1":
+        Console.WriteLine("Saldo");
+        break;
+    case "2":
+        Console.WriteLine("Depósito");
+        break;
+    case "3":
+        Console.WriteLine("Saque");
+        break;
+    default:
+        Console.WriteLine("Opção inválida");
+        break;
+}
---
+++ Fundamentos/08_Estruturas_De_Repeticao/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//FUNDAMENTOS DO C#
+
+//ESTRUTURAS DE REPETIÇÃO
+
+//WHILE
+var contador = 1;
+while (contador <= 10)
+{
+    Console.WriteLine(contador);
+    contador++;
+}
+
+
+//percorrer itens de um array
+var indice = 0;
+var versoes = new[] { "1.0", "1.1", "2.0", "3.0" };
+while (indice < versoes.Length)
+{
+    Console.WriteLine(versoes[indice]);
+    indice++;
+}
+
+
+
+//DO WHILE
+var contador1 = 10;
+var opcao = "";
+do
+{
+    Console.WriteLine("Digite uma opção: ");
+    Console.WriteLine("1 - Saque | 2 - Depósito | 3 - Saldo | 0 - Sair");
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Saque");
+            break;
+        case "2":
+            Console.WriteLine("Depósito");
+            break;
+        case "3":
+            Console.WriteLine("Saldo");
+            break;
+    }
+}
+while (opcao != "0");
+
+
+//FOR 
+for (var indice1 = 0; indice1 < 5; indice1++)
+{
+    Console.WriteLine(indice1);
+}
+
+var numeros = new[] { 1, 10, 100, 1000 };
+for (var indice2 = 0; indice2 < numeros.Length; indice2++)
+{
+    Console.WriteLine(numeros[indice2]);
+}
+
+
+//FOR EACH
+var nomes = new[] { "Isa", "Fulano", "Ciclano" };
+foreach (var item in nomes)
+{
+    Console.WriteLine(item);
+}
